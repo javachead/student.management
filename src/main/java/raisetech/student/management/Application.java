13614ce36bf1fccecb46773b091ad26bbebabb6f
Your Name <you@example.com>
2024-10-09 01:43:04 +0900

    Apache3追加

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -2,12 +2,21 @@ package raisetech.student.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
 }
+
+/* 17_プロジェクトの構成管理_Gradle_Mavenの課題 */
