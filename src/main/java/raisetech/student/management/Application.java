21d2a5937125ed4b5d9587d265b00fa850303fa5
Your Name <you@example.com>
2024-10-09 22:57:34 +0900

    Apache3追加

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -3,20 +3,44 @@ package raisetech.student.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private String name = "Enami Kouji";
+	private String age = "37";
+	private String gender = "Male";
+	private Map<String, String> student = new HashMap<>();
+
+	public Application() {
+		student.put("123", "Alice");
+		student.put("456", "Bob");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + "歳 " + gender;
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(@RequestParam String name, @RequestParam String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@GetMapping("/students")
+	public Map<String, String> getStudents() {
+		return student;
 	}
 }
-
-/* 17_プロジェクトの構成管理_Gradle_Mavenの課題 */
