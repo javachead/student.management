68b71d144a390551158a5be0ba110859227831ca
Your Name <you@example.com>
2024-11-17 00:26:10 +0900

    フォーマットの修正＆gitignoreへの追記

--- src/main/java/raisetech/student/Repository/StudentRepository.java
+++ src/main/java/raisetech/student/Repository/StudentRepository.java
@@ -1,15 +1,17 @@
 package raisetech.student.management;
 
 import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface StudentRepository {
+public
+interface StudentRepository {
 
-	@Select("SELECT * FROM students")
-	List<Student> search();
+    @Select("SELECT * FROM students")
+    List<Student> search();
 
-	@Select("SELECT * FROM student_courses")
-	List<StudentCourse> searchCourseList();
+    @Select("SELECT * FROM student_courses")
+    List<StudentCourse> searchCourseList();
 }
